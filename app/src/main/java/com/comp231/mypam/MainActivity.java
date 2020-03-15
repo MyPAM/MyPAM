32274e7a98076d24a3cf392979d246c01f28d0ea
lnishimaru <lilian.nishimaru@gmail.com>
2020-03-15 17:23:04 -0400

    Implemented Homepage with drawer menu

--- app/src/main/java/com/comp231/mypam/MainActivity.java
+++ app/src/main/java/com/comp231/mypam/MainActivity.java
@@ -2,18 +2,17 @@ package com.comp231.mypam;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Parcelable;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.comp231.mypam.database.DataSource;
 import com.comp231.mypam.model.Category;
@@ -32,59 +31,34 @@ public class MainActivity extends AppCompatActivity {
     private Context mContext;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //creating the database
-        mDataSource = new DataSource(this);
-        mDataSource.open();
-        mDataSource.seedDataBase(categoryList);
 
-        final List<Category> listFromDb = mDataSource.getAllItems();
-        List<String> itemNames = new ArrayList<>();
+    }
 
-        for (Category item: listFromDb) {
-            itemNames.add(item.getCategoryName());
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_category) {
+            /*CategoryFragment fragment = new CategoryFragment();
+            FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();*/
+
+            Intent i = new Intent(getApplicationContext(), CategoryActivity.class);
+            i.putExtra("add", "add");
+            startActivity(i);
         }
 
-        Collections.sort(listFromDb, new Comparator<Category>() {
-            @Override
-            public int compare(Category o1, Category o2) {
-                return o1.getCategoryName().compareTo(o2.getCategoryName());
-            }});
-
-        //DataItemAdapter adapter = new DataItemAdapter(this,listFromDb);
-        DataItemAdapterListView adapter = new DataItemAdapterListView(this,listFromDb);
-        //DataItemAdapterListView adapter = new DataItemAdapterListView(this,categoryList);
-
-        //RecyclerView listView = (RecyclerView) findViewById(android.R.id.list);
-        ListView listView = findViewById(android.R.id.list);
-
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @RequiresApi(api = Build.VERSION_CODES.Q)
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-                                    long arg3) {
-                Intent intent = new Intent(getApplicationContext(),CategoryActivity.class);
-                String message = listFromDb.get(arg2).getCategoryId();
-                intent.putExtra("category", message);
-                startActivity(intent);
-            }
-
-        });
-
-        FloatingActionButton fabAdd = findViewById(R.id.fabAdd);
-
-        fabAdd.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), CategoryActivity.class);
-                i.putExtra("add", "add");
-                startActivity(i);
-            }
-        });
+        return super.onOptionsItemSelected(item);
     }
 }
